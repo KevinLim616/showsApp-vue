8250a60a2bce8e03b28fdf6d48fdc0e0dd3e7de7
KevinLim616 <kevinlim198@gmail.com>
2023-01-01 15:11:16 +0800

    the Suspense tag

---
+++ src/components/BreakingBadCards.vue
@@ -0,0 +1,10 @@
+<script setup>
+import axios from "axios";
+
+const response = await axios.get("https://www.breakingbadapi.com/api");
+</script>
+<template>
+  <div>
+    <h1>{{ response }}</h1>
+  </div>
+</template>
