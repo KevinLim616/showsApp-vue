69826ff0e37f9f3a93f20158947ff84ed028a5c9
KevinLim616 <kevinlim198@gmail.com>
2023-01-01 15:35:33 +0800

    implement paginations

--- src/components/BreakingBadCards.vue
+++ src/components/BreakingBadCards.vue
@@ -1,10 +1,29 @@
 <script setup>
 import axios from "axios";
+import { ref, watch } from "vue";
 
-const response = await axios.get("https://www.breakingbadapi.com/api");
+const characters = ref(null);
+const page = ref(0);
+
+const response = await axios.get(
+  `https://breakingbadapi.com/api/characters?limit=8&offset=8`
+);
+characters.value = response.data;
+
+//whenever click to the next page it renders another 8 items/characters
+watch(page, async () => {
+  const response = await axios.get(
+    `https://breakingbadapi.com/api/characters?limit=8&offset=${page.value * 8}`
+  );
+  characters.value = response.data;
+});
 </script>
 <template>
   <div>
-    <h1>{{ response }}</h1>
+    <h1>{{ characters }}</h1>
+    <div>
+      <button @click="page++">Next</button>
+      <button @click="page--">Back</button>
+    </div>
   </div>
 </template>
